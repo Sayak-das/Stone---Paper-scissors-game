9132a3e988d1401d97de139ad74ad16aa7998027
Sayak Das <108207550+Sayak-das@users.noreply.github.com>
2024-02-22 00:07:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let userScore=0;
+let compScore=0;
+
+const userScorePara=document.querySelector("#user-score");
+const compScorePara=document.querySelector("#comp-score");
+
+const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+
+const generateCompChoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+};
+const drawGame=()=>{
+    msg.innerText="Game draw!";
+    msg.style.backgroundColor="#081b31";
+};
+const showWinner=(userWin)=>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText=userScore;
+        msg.innerText="You win!";
+        msg.style.backgroundColor="green";
+    }
+    else{
+        compScore++;
+        compScorePara.innerText=compScore;
+        msg.innerText="You lose!";
+        msg.style.backgroundColor="red";
+    }
+};
+const playGame=(userChoice)=>{
+    console.log("usr choice",userChoice);
+    const compChoice=generateCompChoice();
+    console.log("comp choice",compChoice);
+
+    if(userChoice===compChoice){
+    drawGame();
+    }
+    else{
+        let userWin=true;
+        if(userChoice==="rock"){
+        userWin=compChoice==="paper"?false:true;
+        }
+        else if(userChoice==="paper"){
+            userWin=compChoice==="scissors"?false:true;
+        }
+        else{
+            userWin=compChoice==="rock"?false:true;
+        }
+        showWinner(userWin);
+    }
+};
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
